74e07a49a236ca4a9a523a819e7664fd3a9958b3
Martin Matysiak <kaktus621@gmail.com>
2011-09-05 22:40:21 +0200

    cleaned gLogger.c; updated changelog

--- src/gLogger.c
+++ src/gLogger.c
@@ -1,100 +1,66 @@
-/*
-  ########## gLogger Mini ##########
-    
-	Von Martin Matysiak
-		mail@martin-matysiak.de
-		www.martin-matysiak.de
-
-	Version 1.30
-
-  ########## Changes: ##########
-
-  Version 1.1 (08.01.2010):
-    * Bugfix: nofs_init scans the whole SD-card for 0x03 (ETX) instead of only
-      the first sector -> Existing data won't be overwritten anymore (thanks to
-      Ralf Fischer for detecting and solving the problem!)
-
-  Version 1.2 (18.07.2011):
-    * Bugfix: It seems that ETX wasn't written correctly. Because of this, the
-      logger couldn't find a place to continue writing. This is fixed now
-      
-  Version 1.3 (29.08.2011):
-    * Bugfix: GPS-Module was set to 2Hz despite of comment saying it was 1Hz,
-      now it's _really_ set to 1Hz
-    * Bugfix: The UART library produced regularly buffer overruns which weren't
-      detected due to the 2Hz bug. This won't happen anymore (thanks to Bastian
-      Wegener!)
-
-  ########## Änderungen: ##########
-
-  Version 1.1 (08.01.2010):
-    * Bugfix: nofs_init durchsucht nun die gesamte SD-Karte nach 0x03 (ETX) an-
-      stelle nur des ersten Sektors -> Bereits vorhandene Aufnahmen werden nicht
-      mehr überschrieben (Vielen Dank an Ralf Fischer für Erkennung und Lösung
-      des Problems!)
-      
-  Version 1.2 (18.07.2011):
-    * Bugfix: Anscheinend wurde das ETX byte nicht ordentlich geschrieben. Des-
-      wegen konnte der Logger keine Position zum Fortsetzen der Aufzeichnung
-      finden. Das ist nun behoben
-      
-  Version 1.3 (29.08.2011):
-    * Bugfix: Das GPS-Modul war, entgegen des Kommentars, auf 2Hz gestellt. Nun
-      ist es _wirklich_ auf 1Hz gestellt.
-    * Bugfix: Die UART Bibliothek produzierte regelmäßig Buffer Überläufe, die
-      wegen des 2Hz-Bugs nicht/kaum bemerkt wurden. Dies ist nun ebenfalls be-
-      hoben (vielen dank an Bastian Wegener!)
-  
-  ########## Licensing ##########
-
-  Please take a look at the LICENSE file for detailed information.
-*/
+/**
+ * \file gLogger.c
+ * \brief The source file containing the main method which is called when
+ * turning on the device.
+ * \author Martin Matysiak
+ */
 
 #include <avr/sleep.h>
 #include "global.h"
 #include "modules/nofs.h"
 #include "modules/gps.h"
 
+/**
+ * \brief Main method of the project
+ *
+ * This method contains the general workflow of the device. This is the place
+ * where the different modules are combined in order to acheive the actual
+ * logging of GPS-coordinates.
+ *
+ * \return Despite of being defined as a function returning an integer, the
+ * method will return nothing, as the code will stay in a while(TRUE)-loop 
+ * prior to the return statement.
+ */
 int main (void) {
+    // Activate interrupts
+    sei();  
 
-	// Interrupts global aktivieren
-  sei();  
+    // Configure ports
+    IO_CONF |= (1 << LED_STAT);
+    LEDCODE_ON();
 
-	// Ports definieren
-	IO_CONF |= (1 << LED_STAT);
-	LEDCODE_ON();
+    // Disable unnecessary functions (Power-reduction)
+    PRR |= (1 << PRTWI) | (1 << PRTIM2) | (1 << PRTIM0) | (1 << PRTIM1) | (1 << PRADC);
 
-  // Unnötige Module ausschalten (Power-reduction)
-  PRR |= (1 << PRTWI) | (1 << PRTIM2) | (1 << PRTIM0) | (1 << PRTIM1) | (1 << PRADC);
+    // Wait a second...
+    _delay_s(1);
 
-  _delay_s(1);
-
-	// Module initalisieren
-  if (!(nofs_init() && gps_init())) {
-    while(1) {
-      _delay_ms(100);
-      LEDCODE_BLINK();    
-    }
-  }
-
-  LEDCODE_OFF();
-
-  char nmeaBuf[128];
-
-  while(1) {
-    // Geschrieben wird nur, wenn NMEA-Kommando einen Fix hatte
-    if(gps_getNmeaSentence(nmeaBuf, 128)) {
-      nofs_writeString(nmeaBuf);
+    // Initialize modules (if either one returns FALSE, the code will terminate
+    // here in the while(TRUE)-loop, signalizing an error to the user)
+    if (!(nofs_init() && gps_init())) {
+        while(TRUE) {
+            _delay_ms(100);
+            LEDCODE_BLINK();    
+        }
     }
 
-    // Blinken !!! (das Wichtigste in diesem ganzen Code)
-    LEDCODE_BLINK();
-    _delay_ms(30);
-    LEDCODE_BLINK(); 
-    sleep_mode();
+    LEDCODE_OFF();
 
-  }
- 
-	// Wird nie erreicht
-	return 0;
+    char nmeaBuf[128];
+
+    while(TRUE) {
+        // We'll write the data only if it contains a valid position
+        if (gps_getNmeaSentence(nmeaBuf, 128)) {
+            nofs_writeString(nmeaBuf);
+        }
+
+        // Flash !!! (the most important part of the code)
+        LEDCODE_BLINK();
+        _delay_ms(30);
+        LEDCODE_BLINK(); 
+        sleep_mode();
+    }
+
+    // Will never be reached
+    return 0;
 }
