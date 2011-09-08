863704b30fa0725e6e858d94fb951db50101fa34
Martin Matysiak <kaktus621@gmail.com>
2011-09-08 15:28:22 +0200

    cleaned spi.h and spi.c

--- src/protocols/spi.h
+++ src/protocols/spi.h
@@ -1,36 +1,58 @@
 /**
-  ########## gLogger Mini ##########
-    
-	Von Martin Matysiak
-	    mail@k621.de
-      www.k621.de
-
-  Befehlsbibliothek für das SPI-Protkoll
-
-  ########## Licensing ##########
-
-  Please take a look at the LICENSE file for detailed information.
-*/
+ * \file spi.h
+ * \brief Library for SPI communication
+ * \author Martin Matysiak
+ */
 
 #ifndef SPI_H
-  #define SPI_H
+    #define SPI_H
 
-  #define SPI_PORT PORTB
-  #define SPI_PORT_DIR DDRB
+    /// The port at which the SPI Pins are
+    #define SPI_PORT PORTB
+    /// The direction register for the SPI Port
+    #define SPI_PORT_DIR DDRB
 
-  #define SPI_CS PB2
-  #define SPI_MOSI PB3
-  #define SPI_MISO PB4
-  #define SPI_SCK PB5
+    /// Chipselect Pin
+    #define SPI_CS PB2
+    /// MOSI Pin
+    #define SPI_MOSI PB3
+    /// MISO Pin
+    #define SPI_MISO PB4
+    /// SCK (CLK) Pin
+    #define SPI_SCK PB5
 
-  #define SET_CS() SPI_PORT &= ~(1 << SPI_CS)
-  #define CLEAR_CS() SPI_PORT |= (1 << SPI_CS)
+    /// Macro to set the Chipselect (i.e. chipselect is pulled to low)
+    #define SET_CS() SPI_PORT &= ~(1 << SPI_CS)
+    /// Macro to clear the Chipselect (i.e. chipselect is pulled to high)
+    #define CLEAR_CS() SPI_PORT |= (1 << SPI_CS)
 
-  #include "global.h"
-  
-  uint8_t spi_init();
+    #include "global.h"
 
-  void spi_writeByte(uint8_t pByte);
-  
-  uint8_t spi_readByte();
+    /**
+     * \brief Initializes the SPI Port.
+     *
+     * Configures the Pin directions and sets a basic configuration in the 
+     * SPI Configuration register.
+     */
+    uint8_t spi_init();
+
+    /**
+     * \brief Sends a byte via SPI
+     * \param pByte The byte to be sent
+     */
+    void spi_writeByte(uint8_t pByte);
+
+    /**
+     * \brief Reads a byte from the SPI and returns it
+     * \return The byte which has been read
+     */
+    uint8_t spi_readByte();
+    
+    /**
+     * \brief Switches SPI Configuration into Highspeed mode
+     *
+     * The speed will be set to the highest possible value, i.e.
+     * F_SPI = F_CPU / 2
+     */
+    void spi_highspeed();
 #endif
