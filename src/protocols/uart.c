027159cb24e049d086308ef3eca6f65b8e39611a
Martin Matysiak <kaktus621@gmail.com>
2011-09-10 22:05:07 +0200

    reduced uart output-buffer size (only took an unnecessarily high amount of RAM)

--- src/protocols/uart.c
+++ src/protocols/uart.c
@@ -16,11 +16,11 @@
 #include <avr/interrupt.h>
 
 //FIFO-Buffer
-static volatile char uart_inputBuf0[UART_BUFFER_SIZE];
+static volatile char uart_inputBuf0[UART_INPUT_BUFFER_SIZE];
 static volatile uint8_t uart_inputBuf0Read = 0;
 static volatile uint8_t uart_inputBuf0Write = 0;
 
-static volatile char uart_outputBuf0[UART_BUFFER_SIZE];
+static volatile char uart_outputBuf0[UART_OUTPUT_BUFFER_SIZE];
 static volatile uint8_t uart_outputBuf0Read = 0;
 static volatile uint8_t uart_outputBuf0Write = 0;
 
@@ -39,7 +39,7 @@ void uart_init(uint8_t pConfig, uint16_t pUbr) {
 unsigned char uart_getChar() {
   if (uart_inputBuf0Read != uart_inputBuf0Write) {
     // Increment reading pointer while catching a possible array overflow
-    if (++uart_inputBuf0Read >= UART_BUFFER_SIZE) {
+    if (++uart_inputBuf0Read >= UART_INPUT_BUFFER_SIZE) {
       uart_inputBuf0Read = 0;
     }
   
@@ -73,7 +73,7 @@ uint8_t uart_getString(char* pResult, uint8_t pResultSize) {
 void uart_setChar(char pData) {
   //Byte in den Sendebuffer schreiben, Sendeinterrupt aktivieren
   //Schreibzeiger um Eins vorrücken, wenn dann auf Lesezeiger liegt, warten
-  if (uart_outputBuf0Write+1 >= UART_BUFFER_SIZE) {
+  if (uart_outputBuf0Write+1 >= UART_OUTPUT_BUFFER_SIZE) {
     // writing pointer is at the end of the buffer array, next index will be 0  
     while (uart_outputBuf0Read == 0) {
       // wait, buffer is full
@@ -108,7 +108,7 @@ void uart_clearBuf() {
 
 //Interrupt-Abarbeitung
 ISR(USART_RX_vect) {
-  if(uart_inputBuf0Write+1 >= UART_BUFFER_SIZE) {
+  if(uart_inputBuf0Write+1 >= UART_INPUT_BUFFER_SIZE) {
     // writing pointer is at the end of the buffer array, next index will be 0
     if(uart_inputBuf0Read != 0) {
         uart_inputBuf0Write = 0;
@@ -131,7 +131,7 @@ ISR(USART_RX_vect) {
 ISR(USART_UDRE_vect) {
   //Solange nächstes Byte schreiben, bis Lese == Schreibposition
   if (uart_outputBuf0Read != uart_outputBuf0Write) {
-    if (++uart_outputBuf0Read >= UART_BUFFER_SIZE) {
+    if (++uart_outputBuf0Read >= UART_OUTPUT_BUFFER_SIZE) {
       uart_outputBuf0Read = 0;
     }
   
