7da703fb167689dd273adfc1a1ae8e9a6ab3b1e6
Martin Matysiak <kaktus621@gmail.com>
2011-09-17 17:37:30 +0200

    implemented the first few nofs_init steps

--- src/modules/nofs.c
+++ src/modules/nofs.c
@@ -20,16 +20,37 @@ void nofs_init() {
         Steps of initialization:
         1) Call initialization of underlying SDMMC interface
         2) Read the first sector
-        3) Check if sector starts with NOFS_HEADER (return FALSE if not)
+        3) Check if sector starts with NOFS_HEADER (display error if not)
         4) Get position of last scan for writing position
         5) Jump to this position and scan for NOFS_TERMINAL from there on
         6) Jump back to first sector and update last writing position
-        7) Jump forward to writing position, return TRUE
+        7) Jump forward to writing position, initialization finished.
     */
     
     // Step 1
     sdmmc_init();
     
+    // Step 2
+    sdmmc_readSector(0, sectorBuf);
+    
+    // Step 3
+    if (!strStartsWith(sectorBuf, NOFS_HEADER)) {
+        error(ERROR_NOFS);
+    }
+    
+    // Step 4
+    // TODO //
+    
+    // Step 5
+    // TODO //
+    
+    // Step 6
+    // TODO //
+    
+    // Step 7
+    // TODO //
+    
+    // Old code:
     do {
         // Search for the end of data (== ETX, 0x03) on the memory card, which
         // will be the position from where we will start writing
