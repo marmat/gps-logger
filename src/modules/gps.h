71f9b717b550759afe75ed093e016b76969836ad
Martin Matysiak <kaktus621@gmail.com>
2011-11-01 02:44:24 +0100

    bugfix: replaced binary with hex notation in constant definitions (some compilers couldn't process it)

--- src/modules/gps.h
+++ src/modules/gps.h
@@ -38,13 +38,13 @@
     #define GPS_NMEA_INVALID 0x00
 
     // Possible NMEA-Messages that may be returned
-    #define GPS_NMEA_GGA 0b00000010 // Note: bit 0 can't be used, is an indicator
-    #define GPS_NMEA_GSA 0b00000100 // for validity of the message
-    #define GPS_NMEA_GSV 0b00001000
-    #define GPS_NMEA_GLL 0b00010000
-    #define GPS_NMEA_RMC 0b00100000
-    #define GPS_NMEA_VTG 0b01000000
-    #define GPS_NMEA_ZDA 0b10000000
+    #define GPS_NMEA_GGA 0x02 // 0b00000010 // Note: bit 0 can't be used, is an indicator
+    #define GPS_NMEA_GSA 0x04 // 0b00000100 // for validity of the message
+    #define GPS_NMEA_GSV 0x08 // 0b00001000
+    #define GPS_NMEA_GLL 0x10 // 0b00010000
+    #define GPS_NMEA_RMC 0x20 // 0b00100000
+    #define GPS_NMEA_VTG 0x40 // 0b01000000
+    #define GPS_NMEA_ZDA 0x80 // 0b10000000
 
     /// The value which is returned when no known NMEA-command has been recognized
     #define GPS_NMEA_UNKNOWN 0
