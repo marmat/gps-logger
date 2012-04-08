8b0b5dc1d01b94936d86f6aa63b6c5740f29baba
Martin Matysiak <kaktus621@gmail.com>
2012-04-08 14:52:09 +0200

    updated blinking routine
    Signed-off-by: Martin Matysiak <kaktus621@gmail.com>

--- src/gLogger.c
+++ src/gLogger.c
@@ -10,6 +10,32 @@
 #include "modules/nofs.h"
 #include "modules/gps.h"
 
+////////////////////////////////////////////////////////////////////////////////
+// Change these constans in order to alter the logging behaviour
+
+/**
+ * Indicates how many message packets per second shall be recorded 
+ * Valid values for the ST22 are: [1, 2, 4, 8, 10]
+ */
+#define FREQUENCY 1
+
+/**
+ * Indicates which messages to record per message packet
+ * Valid values for the ST22 are:
+ * [GPS_NMEA_GGA, GPS_NMEA_GSA, GPS_NMEA_GSV, GPS_NMEA_GLL, GPS_NMEA_RMC,
+ *  GPS_NMEA_VTG, GPS_NMEA_ZDA]
+ */
+#define MESSAGES GPS_NMEA_GGA | GPS_NMEA_RMC | GPS_NMEA_VTG
+
+// No changes needed after this point
+////////////////////////////////////////////////////////////////////////////////
+
+/// Number of set bits in the MESSAGES field (ugly code, but it serves its purpose)
+#define NUM_MESSAGES (((MESSAGES) & 1) + (((MESSAGES) >> 1) & 1) + (((MESSAGES) >> 2) & 1) + (((MESSAGES) >> 3) & 1) + (((MESSAGES) >> 4) & 1) + (((MESSAGES) >> 5) & 1) + (((MESSAGES) >> 6) & 1) + (((MESSAGES) >> 7) & 1))
+
+/// The LED will blink every LED_THRESHOLD messages
+#define LED_THRESHOLD NUM_MESSAGES * FREQUENCY
+
 char nmeaBuf[128];
 
 /**
@@ -40,25 +66,28 @@ int main (void) {
     // Initialize the necessary modules (these methods may lock the processor
     // in an endless loop if an error occurs!)
     nofs_init();
-    gps_init(10, 
-        GPS_NMEA_GGA | GPS_NMEA_GSA | 
-        GPS_NMEA_GSV | GPS_NMEA_GLL | 
-        GPS_NMEA_RMC | GPS_NMEA_VTG | 
-        GPS_NMEA_ZDA);
+    gps_init(FREQUENCY, MESSAGES);
 
     // Write a short information string containing the firmware version (NMEA compliant)
-    nofs_writeString("\r\n$PGLGVER,1.6a2\r\n");
+    nofs_writeString("\r\n$PGLGVER,1.6\r\n");
 
+    // Keep track of received messages
+    uint8_t messageCount = 0;
     LEDCODE_OFF();
 
     while(1) {
         // We'll write the data only if it contains a valid position
-        if (gps_getNMEA(nmeaBuf, 128)) {
+        if (gps_getNMEA(nmeaBuf, 128) & GPS_NMEA_VALID) {
             nofs_writeString(nmeaBuf);
         }
 
-        // Flash !!! (the most important part of the code)
-        LEDCODE_BLINK();
+        if (++messageCount == LED_THRESHOLD) {
+            // Flash !!! (the most important part of the code)
+            LEDCODE_BLINK();
+            _delay_ms(30);
+            LEDCODE_BLINK();
+            messageCount = 0;
+        }        
         
         sleep_mode();
     }
